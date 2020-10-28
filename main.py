eccf47f56072914508d297a010994129ca50eb2b
PatrikVerseghy <patrik.verseghy@gmail.com>
2020-10-28 14:54:46 +0100

    Add final changes

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def get_artist_name_dates():
 
 
 def main():
-    # get a file with only artists
+    # get a file which contains only artists
     get_artist_file()
     # get files of names, dates of birth and dates of death
     get_artist_name_dates()
